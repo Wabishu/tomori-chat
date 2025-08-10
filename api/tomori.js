8118abca1eaf712b3d7dd81b261b693a3c127490
Wabishu <hvl.coral@gmail.com>
2025-08-11 03:43:32 +0900

    Update tomori.js

--- api/tomori.js
+++ api/tomori.js
@@ -1,5 +1,13 @@
 // /api/tomori.js
 export default async function handler(req, res) {
+//
+  if (req.method === 'GET') {
+    return res.status(200).json({ ok: true });
+  }
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }  
+  //
   try {
     if (req.method !== 'POST') {
       return res.status(405).json({ reply: 'Method Not Allowed' });
